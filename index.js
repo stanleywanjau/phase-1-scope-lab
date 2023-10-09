0b1c7febe057b0223b1aa1e50966b4ef35d035f3
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-09 12:41:40 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+var upperCaseCustomerName = function(){
+    customerName=customerName.toUpperCase();
+}
+
+var setBestCustomer = function(){
+    bestCustomer = "not bob";
+}
+
+var overwriteBestCustomer = function(newBestCustomer) {
+    bestCustomer = newBestCustomer
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new least favorite";
+  }
